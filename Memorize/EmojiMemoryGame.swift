54571b5d3158048402acb962dc26f32dce7b4152
bowen <bowen.zhang@sidus.link>
2021-12-15 18:04:10 +0800

    更新

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Bowen on 2021/12/15.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    privite(set) var model: MemoryGame<String> = MemoryGame<String>()
+}
--- Memorize/MemorizeGame.swift
+++ Memorize/MemorizeGame.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
+    init(numberOfPairsOfCards: Int){
+        
+    }
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
